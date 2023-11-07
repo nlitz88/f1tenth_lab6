b169773b8084e97c3e3b147b13566600432b3446
Nathan Litzinger <nlitz88@gmail.com>
2023-11-07 11:14:15 -0500

    Created new sampling function that uses the pre-generated free space array

--- lab6_pkg/lab6_pkg/rrt_utils.py
+++ lab6_pkg/lab6_pkg/rrt_utils.py
@@ -14,46 +14,37 @@ def free_space_from_costmap(costmap: np.ndarray,
 
     Returns:
         np.ndarray: The 2D array, which can be thought of as a list of tuples
-        essentially, where each tuple is the X,Y coordinates of a free space in
+        essentially, where each tuple is the x,y coordinates of a free space in
         the provided costmap.
     """
     return np.transpose(np.flip(np.where(costmap < occupied_threshold)))
 
-# def sample(costmap: np.ndarray,
-#            occupied_threshold: Optional[int] = 100) -> Tuple[int, int]:
-#     """Returns the coordinates of a random point in the unoccupied space of the
-#     provided 2D costmap. Throws an exception if there are no free spaces to
-#     sample from.
+def sample(free_space: np.ndarray) -> Tuple[int, int]:
+    """Returns the coordinates of a random point in the unoccupied space of the
+    provided 2D costmap. Throws an exception if there are no free spaces to
+    sample from.
 
-#     Args:
-#         costmap (np.ndarray): 2D array derived from an a costmap occupancy grid.
-#         occupied_threshold (Optional[int], optional): The cost that a cell must be
-#         in order to be considered occupied space. Defaults to 100.
+    Args:
+        free_space (np.ndarray): A 2D array that contains (intuitively) a list
+        of all the coordinates in the costmap that are not occupied.
 
-#     Raises:
-#         Exception: Raises exception if there are no free spaces for it to sample
-#         from.
+    Raises:
+        Exception: Raises exception if there are no free spaces for it to sample
+        from.
 
-#     Returns:
-#         Tuple[int, int]: Returns the coordinates of the sampled space as a tuple
-#         (x,y).
-#     """
-#     # 1. Use np.where to get the x and y coordinates of the locations where the
-#     #    cost is less than the cost threshold. Take the transpose to get an
-#     #    array of [x,y] arrays, rather than two separate, parallel x and y index
-#     #    arrays. Have to flip so that x coords (columns) are on top.
-#     # TODO: May want to separate this out into a separate block or something and
-#     # pass it in, as we're going to have to sample from this free_coords array
-#     # repeatedly.
-    
-#     if free_coords.shape[0] == 0:
-#         raise Exception("No free spaces found to sample from in costmap.")
-#     # 2. Choose a random number between 0 and the length of the free coordinates
-#     #    list. Use a uniform distribution, giving equal probability of picking
-#     #    all points in free space.
-#     random_index = np.random.randint(low=0, high=free_coords.shape[0])
-#     # 3. Return the coordinates at that index.
-#     return tuple(free_coords[random_index])
+    Returns:
+        Tuple[int, int]: Returns the coordinates of the sampled space as a tuple
+        (x,y).
+    """
+    # 1. Check if there are any free spaces to sample from.
+    if free_space.shape[0] == 0:
+        raise Exception("Free space sampling failed--no free spaces found to sample from in costmap!")
+    # 2. Choose a random number between 0 and the length of the free coordinates
+    #    list. Use a uniform distribution, giving equal probability of picking
+    #    all points in free space.
+    random_index = np.random.randint(low=0, high=free_space.shape[0])
+    # 3. Return the coordinates at that index.
+    return tuple(free_space[random_index])
 
 def nearest(self, tree, sampled_point):
     """
--- lab6_pkg/lab6_pkg/tests/test_rrt_utils.py
+++ lab6_pkg/lab6_pkg/tests/test_rrt_utils.py
@@ -1,32 +1,6 @@
 import unittest
 from lab6_pkg.rrt_utils import *
 
-# class TestSampleFunction(unittest.TestCase):
-
-#     def test_fully_occupied(self):
-#         costmap = np.array([[150, 150], [150, 150]]) # costmap with all cells occupied
-#         with self.assertRaises(Exception):
-#             sample(costmap)
-
-#     def test_some_free_spaces(self):
-#         costmap = np.array([[150, 50], [150, 150]]) # costmap with some free spaces
-#         x, y = sample(costmap)
-#         self.assertTrue(0 <= x < costmap.shape[0] and 0 <= y < costmap.shape[1])
-
-#     def test_different_sizes(self):
-#         costmap1 = np.array([[50, 50], [50, 50]]) # 2x2 costmap
-#         costmap2 = np.array([[50, 50, 50], [50, 50, 50], [50, 50, 50]]) # 3x3 costmap
-#         costmap3 = np.array([[50, 50, 50, 50], [50, 50, 50, 50], [50, 50, 50, 50], [50, 50, 50, 50]]) # 4x4 costmap
-#         for costmap in [costmap1, costmap2, costmap3]:
-#             x, y = sample(costmap)
-#             self.assertTrue(0 <= x < costmap.shape[0] and 0 <= y < costmap.shape[1])
-
-#     def test_one_free_space(self):
-#         costmap = np.array([[150, 150], [50, 150]]) # costmap with one free space
-#         x, y = sample(costmap)
-#         self.assertTrue((x, y) == (1, 0))
-
-
 class TestFreeSpaceFromCostmap(unittest.TestCase):
     def test_empty_costmap(self):
         costmap = np.array([])
@@ -52,6 +26,23 @@ class TestFreeSpaceFromCostmap(unittest.TestCase):
         result = free_space_from_costmap(costmap, occupied_threshold)
         expected_result = np.flip(np.array([[0, 0], [0, 2], [1, 1], [2,0], [2, 2]]))
         self.assertTrue(np.array_equal(result, expected_result))
+
+class TestSample(unittest.TestCase):
+    def test_empty_free_space(self):
+        free_space = np.array([])
+        with self.assertRaises(Exception) as context:
+            sample(free_space)
+        self.assertTrue("Free space sampling failed--no free spaces found to sample from in costmap!" in str(context.exception))
+
+    def test_single_free_space(self):
+        free_space = np.array([[1, 1]])
+        result = sample(free_space)
+        self.assertEqual(result, (1, 1))
+
+    def test_multiple_free_space(self):
+        free_space = np.array([[0, 0], [1, 1], [2, 2], [3, 3]])
+        result = sample(free_space)
+        self.assertTrue(result in [(0, 0), (1, 1), (2, 2), (3, 3)])
 
 if __name__ == "__main__":
     unittest.main()
